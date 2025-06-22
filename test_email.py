52b79f7f63dde492c88eb205cad93e35006cdf10
Patrik Nordlund <130709148+epatnor@users.noreply.github.com>
2025-06-22 13:55:19 +0200

    Update test_email.py

--- test_email.py
+++ test_email.py
@@ -1,12 +1,15 @@
 import smtplib
 
 smtp_server = "smtp.gmail.com"
-smtp_port = 465
+smtp_port = 587
 smtp_username = "epatnor@gmail.com"
 smtp_password = "ebxkozpuvxpvgauq"
 
 try:
-    with smtplib.SMTP_SSL(smtp_server, smtp_port) as server:
+    with smtplib.SMTP(smtp_server, smtp_port) as server:
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
         server.login(smtp_username, smtp_password)
         print("SMTP login success!")
 except Exception as e:
