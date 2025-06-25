ecbb109f766ed108defdeebd0c83d31efa6868a4
Patrik Nordlund <130709148+epatnor@users.noreply.github.com>
2025-06-25 10:55:21 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -3,6 +3,7 @@ setlocal enabledelayedexpansion
 
 cd /d %~dp0
 
+echo.
 echo ================================
 echo Updating repository...
 git pull
@@ -12,17 +13,24 @@ echo ================================
 if not exist ".venv\" (
     echo Creating virtual environment...
     python -m venv .venv
+    if %ERRORLEVEL% neq 0 (
+        echo Failed creating venv!
+        pause
+        exit /b 1
+    )
 )
 
 call .venv\Scripts\activate
 
+echo.
+echo ================================
 echo Installing requirements...
 pip install --upgrade pip
 pip install -r requirements.txt
+echo ================================
 
+echo.
 echo Starting backend server...
-start "Backend Server" cmd /k uvicorn api:app --reload
-
-echo Open http://localhost:8000 in your browser!
+uvicorn api:app --reload
 
 endlocal
